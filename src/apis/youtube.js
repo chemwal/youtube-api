536c2230e5827920d009e353b7000a4addf47a29
Ujjwal <manitripathiujjwal@gmail.com>
2021-11-03 09:08:40 +0530

    Final Commit

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = 'AIzaSyDzhRdg4ELBW5iL5CWa9ShSuOZXpBp8vdQ';
+const KEY = 'PLACE KEY HERE';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
